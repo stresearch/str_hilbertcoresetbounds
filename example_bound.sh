13665594da189e700e2562dcd48d9567431b2217
sda0428 <spencer.douglas@stresearch.com>
2021-08-20 17:02:29 -0400

    Updated example script

--- example_bound.sh
+++ example_bound.sh
@@ -6,4 +6,4 @@
 # test_results.npz: The output of the bound computation
 # str_lwll.log: The log statements from the computation
 echo Even this toy example can easily take over an hour
-python3 coreset_calc_script.py -m StrMnist -c 500 -e hess.npy -l . --sigma-prior 0.0001 -j 10 -n 5 -k . -g 0 -r test_results.npz -o Debug
+python3 coreset_calc_script.py -m StrMnist -c 500 -e bound_calculation/example_bound/hess.npy -l bound_calculation/example_bound/sample_loc/ --sigma-prior 0.0001 -j 10 -n 5 -k . -g 0 -r bound_calculation/example_bound/test_results.npz -o Debug
